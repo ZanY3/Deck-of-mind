bb730613570b37dfc50efc4380b8e4cf4a1a69f0
Denis Baran <dendombar221@gmail.com>
2026-02-24 17:21:26 +0200

    Mini boss added - Denial
    He is canceling all of skills

--- Assets/Game/Scripts/Enemies/EnemyDropTarget.cs
+++ Assets/Game/Scripts/Enemies/EnemyDropTarget.cs
@@ -1,10 +1,16 @@
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.EventSystems;
 using DG.Tweening;
 using Unity.VisualScripting;
 
 public class EnemyDropTarget : MonoBehaviour
 {
+    [Header("Not required")]
+    [SerializeField] private GameObject skillCanceledClue;
+    [SerializeField] private bool immuneToDebuffs;
+    [SerializeField] private AudioClip deniedSound;
+    [SerializeField] private float deniedVolume;
+
     [HideInInspector] public bool canBeAttacked = true;
     private Enemy enemy;
     private CardEffects effects;
@@ -34,31 +40,54 @@ public class EnemyDropTarget : MonoBehaviour
         }
         else if (card.type == CardData.CardType.SkillOnEnemy)
         {
-            switch (card.effect)
+            if(immuneToDebuffs == true)
             {
-                case CardData.Effect.Stun:
-                    if (!enemy.stunned)
-                    {
-                        enemy.transform.DOShakeScale(0.15f, new Vector3(0.15f, 0.15f, 0)).SetAutoKill(true).SetUpdate(true);
-                        effects.Stun(enemy);
-                    }
-                    break;
+                SoundManager.Instance.PlaySFX(deniedSound, Random.Range(0.95f, 1.05f), deniedVolume);
+                skillCanceledClue.SetActive(true);
 
-                case CardData.Effect.HealthDrain:
-                    if (!enemy.hpWeakened && enemy.currentHealth != 1)
-                    {
-                        enemy.transform.DOShakeScale(0.15f, new Vector3(0.15f, 0.15f, 0)).SetAutoKill(true).SetUpdate(true);
-                        effects.HealthWeaken(enemy, true, 2);
-                    }
-                    break;
+                CanvasGroup cg = skillCanceledClue.GetComponent<CanvasGroup>();
+                if (cg == null)
+                    cg = skillCanceledClue.AddComponent<CanvasGroup>();
 
-                case CardData.Effect.StrengthDrain:
-                    if (!enemy.strengthWeakened && enemy.damage != 1)
-                    {
-                        enemy.transform.DOShakeScale(0.15f, new Vector3(0.15f, 0.15f, 0)).SetAutoKill(true).SetUpdate(true);
-                        effects.StrengthWeaken(enemy, true, 2);
-                    }
-                    break;
+                cg.alpha = 1f;
+
+                // Через 2 секунды плавно исчезает
+                cg.DOFade(0f, 0.5f)
+                  .SetDelay(1f)
+                  .OnComplete(() =>
+                  {
+                      skillCanceledClue.SetActive(false);
+                      cg.alpha = 1f; // сброс альфы для следующего показа
+                  });
+            }
+            else
+            {
+                switch (card.effect)
+                {
+                    case CardData.Effect.Stun:
+                        if (!enemy.stunned)
+                        {
+                            enemy.transform.DOShakeScale(0.15f, new Vector3(0.15f, 0.15f, 0)).SetAutoKill(true).SetUpdate(true);
+                            effects.Stun(enemy);
+                        }
+                        break;
+
+                    case CardData.Effect.HealthDrain:
+                        if (!enemy.hpWeakened && enemy.currentHealth != 1)
+                        {
+                            enemy.transform.DOShakeScale(0.15f, new Vector3(0.15f, 0.15f, 0)).SetAutoKill(true).SetUpdate(true);
+                            effects.HealthWeaken(enemy, true, 2);
+                        }
+                        break;
+
+                    case CardData.Effect.StrengthDrain:
+                        if (!enemy.strengthWeakened && enemy.damage != 1)
+                        {
+                            enemy.transform.DOShakeScale(0.15f, new Vector3(0.15f, 0.15f, 0)).SetAutoKill(true).SetUpdate(true);
+                            effects.StrengthWeaken(enemy, true, 2);
+                        }
+                        break;
+                }
             }
         }
     }
